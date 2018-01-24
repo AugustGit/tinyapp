7bcc946231543ae9c6d9a93bbbf9457a2d80d08a
austin metka <austin_metka@hotmail.com>
2018-01-24 03:56:20 +0000

    just realized was saving one folder down and unsure how this will effect commits so I am recommitting

--- expressServe/expressEx.js
+++ expressServe/expressEx.js
@@ -4,12 +4,61 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs")
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+function generateRandomString() {
+  var anysize = 6;//the size of string
+  var charset = "ABCDEFGHIGKLMNOPQURSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789"; //from where to create
+  result= "";
+     for( var i=0; i < anysize; i++ )
+        result += charset[Math.floor(Math.random() * charset.length)];
+    return result;
+}
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  let longURLKeyValue = req.body
+  let longURL = longURLKeyValue['longURL']
+  console.log("trying to get value " + longURL)
+  let shortURL = generateRandomString()
+
+
+  console.log(" random string " + shortURL)
+  //console.log(longURL)
+  urlDatabase[shortURL] = longURL
+  //console.log(req.body);
+  console.log(urlDatabase)
+   //{ longURL: 'https://www.pinterest.ca' }
+   // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+app.get("/u/:shortURL", (req, res) => {
+  // let longURL = ...
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+
+
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
